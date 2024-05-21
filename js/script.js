38a7eafc17f9784f47c4217a93187529f37697d2
Dihep <dihep.demayo@gmail.com>
2024-05-22 00:59:57 +0200

    M1- Stampa di array in pagina come lista.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            placeholderData: `Placeholder`
+            toDoList: [
+                `Cosa da fare 1`,
+                `Cosa da fare 2`,
+                `Cosa da fare 3`,
+                `Cosa da fare 4`,
+                `Cosa da fare 5`,
+                `Cosa da fare 6`,
+                `Cosa da fare 7`,
+                `Cosa da fare 8`,
+                `Cosa da fare 9`,
+            ]
         }
     },
     methods: {
